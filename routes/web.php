c212b9100191996ff595a44ec689ffb5cb6024aa
7117 <qinglangsalaheiyou@163.com>
2020-01-25 11:16:11 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
+use function foo\func;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', function () {
+    return 'post';
+});
+
+Route::match(['get', 'post'], 'match', function () {
+    return 'match';
+});
+
+
+Route::any('any', function () {
+    return 'any';
+});
+
+Route::get('para/{id}', function ($id) {
+    return "para/{$id}";
+});
+
+//自定义的默认参数
+Route::get('para/i/{name2?}', function ($name2 = 'sun') {
+    return "para/i/{$name2}";
+})->where('name2','[A-Za-z]+');
+
+//自定义的参数
+Route::get('para/name/{name2?}', function ($name2 = 'sun') {
+    return "para/name/{$name2}";
+})->where('name2', '[A-Za-z]+');
+
+//自定义两个参数
+Route::get('para/name2/{name2?}/{id?}', function ($name2 = 'sun',$id='111') {
+    return "para/name/{$name2}/{$id}";
+})->where(['name2'=>'[A-Za-z]+','id'=>'[0-9]+']);
+
+
+
+
